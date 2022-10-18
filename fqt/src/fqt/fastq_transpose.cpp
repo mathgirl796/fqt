89a558b1839659392f9926f9582b2e0b5aa42408
duanran <aduan456@126.com>
2022-10-18 17:12:50 +0800

    finish barcode generator, todo: test it

--- fqt/src/fqt/fastq_transpose.cpp
+++ fqt/src/fqt/fastq_transpose.cpp
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <zlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <errno.h>
 #include <time.h>
-#include "fqt.h"
-#include "../clib/utils.h"
+#include "fastq_transpose.hpp"
+extern "C" {
+    #include "../clib/utils.h"
+}
 
-int fqt(char *file_path, char *output_dir, int buf_size)
+int fastq_transpose(char *file_path, char *output_dir, int buf_size)
 {
     /* define temp variables */
     gzFile fq_gzfile = gzopen(file_path, "r");
@@ -17,6 +20,7 @@ int fqt(char *file_path, char *output_dir, int buf_size)
     gzFile *q_ofs; /* output files, totally read_len files */
     char **s_bufs; /* buffers for each file */
     char **q_bufs; /* buffers for each file */
+    char fn[1024];
     int c = 0;
     clock_t t = clock();
     while ((l = kseq_read(seq)) >= 0)
@@ -25,9 +29,9 @@ int fqt(char *file_path, char *output_dir, int buf_size)
         if (seq_num == 0)
         {
             /* open files */
-            s_ofs = xmalloc(l * sizeof(gzFile));
-            q_ofs = xmalloc(l * sizeof(gzFile));
-            char fn[1024];
+            s_ofs = (gzFile *)xmalloc(l * sizeof(gzFile));
+            q_ofs = (gzFile*)xmalloc(l * sizeof(gzFile));
+            
             for (int i = 0; i < l; ++i)
             {
                 sprintf(fn, "%s/S_%03d.bin.gz", output_dir, i);
@@ -36,12 +40,12 @@ int fqt(char *file_path, char *output_dir, int buf_size)
                 q_ofs[i] = gzopen(fn, "w");
             }
             /* allocate buffers */
-            s_bufs = xmalloc(l * sizeof(char *));
-            q_bufs = xmalloc(l * sizeof(char *));
+            s_bufs = (char**)xmalloc(l * sizeof(char *));
+            q_bufs = (char**)xmalloc(l * sizeof(char *));
             for (int i = 0; i < l; ++i)
             {
-                s_bufs[i] = xmalloc(buf_size * sizeof(char));
-                q_bufs[i] = xmalloc(buf_size * sizeof(char));
+                s_bufs[i] = (char*)xmalloc(buf_size * sizeof(char));
+                q_bufs[i] = (char*)xmalloc(buf_size * sizeof(char));
             }
         }
         seq_num++;
@@ -79,6 +83,14 @@ int fqt(char *file_path, char *output_dir, int buf_size)
             seq_num, (float)(clock()-t)/CLOCKS_PER_SEC);
     kseq_destroy(seq);
     gzclose(fq_gzfile);
+
+    // write meta infomation
+    sprintf(fn, "%s/meta.txt", output_dir);
+    FILE *f = fopen(fn, "w");
+    err_fprintf(f, "%d", seq_num);
+    err_fclose(f);
+
+    // free resource
     for (int i = 0; i < read_len; ++i)
     {
         free(s_bufs[i]);
---
+++ fqt/src/main.cpp
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <string.h>
+#include <zlib.h>
+#include <time.h>
+#include "fqt/fastq_transpose.hpp"
+#include "fqt/barcode_generate.hpp"
+
+int transpose_main(int argc, char** argv) {
+	int buf_size = 1024 * 1024;
+	char output_dir[1024];
+	size_t len;
+
+	char ch;
+	while ((ch = (char)getopt(argc, argv, "b:o:")) != EOF)
+	{
+		switch (ch)
+		{
+		case 'b':
+			buf_size = atoi(optarg);
+			break;
+		case 'o':
+			strcpy(output_dir, optarg);
+			len = strlen(output_dir);
+			if (output_dir[len - 1] != '/') strcat(output_dir, "/");
+			break;
+		default:
+			return 1;
+		}
+	}
+	// run
+	return fastq_transpose(argv[optind], output_dir, buf_size);
+}
+
+int concat_main(int argc, char** argv) {
+	int buf_size = 1024 * 1024;
+	int lreads;
+	char output_dir[1024];
+	size_t len;
+
+	char ch;
+	while ((ch = (char)getopt(argc, argv, "b:r:o:")) != EOF)
+	{
+		switch (ch)
+		{
+		case 'b':
+			buf_size = atoi(optarg);
+			break;
+		case 'r':
+			lreads = atoi(optarg);
+		case 'o':
+			strcpy(output_dir, optarg);
+			len = strlen(output_dir);
+			if (output_dir[len - 1] != '/') strcat(output_dir, "/");
+			break;
+		default:
+			return 1;
+		}
+	}
+
+	return barcode_simulate(argv+optind, output_dir, buf_size, lreads, argc-optind);
+}
+
+int main(int argc, char **argv)
+{
+
+	if (argc <= 1);
+	else if (strcmp(argv[1], "transpose") == 0) return transpose_main(argc - 1, argv + 1);
+	else if (strcmp(argv[1], "concat") == 0) return concat_main(argc - 1, argv + 1);
+
+	printf("command error\n");
+
+	return 0;
+}
